16df25e96dc99b755cc445fcbb120546fec98441
Andrew <dinogalaktika@gmail.com>
2024-01-23 23:40:21 +0100

    changes in decorations. text-left

--- app/projects/[project]/page.jsx
+++ app/projects/[project]/page.jsx
@@ -21,7 +21,7 @@ export default async function Project({ params }) {
             <div className="flex-1 grid grid-cols-1 md:grid-cols-3 p-4 gap-4 items-center max-w-3xl mx-auto text-center ">
                 <div className="col-span-1 md:col-span-3 text-center lg:mx-20    mt-7">
                     <h1 className="text-2xl font-semibold mb-4">{project.name}</h1>
-                    <div className="mb-16">
+                    <div className="mb-16 text-left">
                         <SanityBlockContent blocks={project.content} />
                     </div>
             
